80414264ab3a60f8d64e9750976fd7d4d2b5d3f6
Anthony Talarico <astalarico@gmail.com>
2022-03-22 19:38:12 -0400

    fixing model create command include path

--- src/commands/ModelCreateCommand.php
+++ src/commands/ModelCreateCommand.php
@@ -5,6 +5,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command as SymfonyCommand;
+use Ashiba\ClassFromStub;
 
 class ModelCreateCommand extends SymfonyCommand {
 
